5c9aac96e721796c604ed9e0db410f179d15551b
Ajithxolo <ajithbuddy.kumar@gmail.com>
2025-02-11 22:00:08 +0530

    Added code to returns sums when the input is a string with a custom delimiter and two 3-digit numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,17 +1,21 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-
-    calculate_sum(parse_input(numbers))
+    delimiter, numbers = parse_input(numbers)
+    calculate_sum(numbers, delimiter)
   end
 
   private
 
   def parse_input(numbers)
-    numbers.split(/[\n,]/).map(&:to_i)
+    return [",", numbers] unless numbers.start_with?("//")
+    delimiter, numbers = numbers.split("\n")
+    parsed_delimiter = delimiter[2..-1]
+    [parsed_delimiter, numbers]
   end
 
-  def calculate_sum(numbers)
-    numbers.sum
+  def calculate_sum(numbers, delimiter)
+    regex = /#{Regexp.escape(delimiter)}|\n/
+    numbers.split(regex).map(&:to_i).sum 
   end
 end
