5a317feb854035d354d4b0b4bab1d42f959df27b
Ajithxolo <ajithbuddy.kumar@gmail.com>
2025-02-11 21:29:21 +0530

    Adding code to returns the sum when the input is a string of numbers separated by commas

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,12 +2,16 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    parse_input(numbers)
+    calculate_sum(parse_input(numbers))
   end
 
   private
 
   def parse_input(numbers)
-    numbers.to_i
+    numbers.split(',').map(&:to_i)
+  end
+
+  def calculate_sum(numbers)
+    numbers.sum
   end
 end
