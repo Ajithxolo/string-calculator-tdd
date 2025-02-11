ae6370084e3f4995b182cce6f27064c4ced354f4
Ajithxolo <ajithbuddy.kumar@gmail.com>
2025-02-11 21:12:54 +0530

    Added code to returns the same number when the input is a string with a single number

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,13 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
+
+    parse_input(numbers)
+  end
+
+  private
+
+  def parse_input(numbers)
+    numbers.to_i
   end
 end
